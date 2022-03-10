b5da148f43f228f8ed18080836de14aacdd963f3
Patrick O'Connor <oconnor.p2016@gmail.com>
2022-03-10 13:00:43 -0700

    Initial attempt at MarginValidator.cs. No major features need to be pulled

--- SourceCode/ETDValidator/ETDValidator/Models/Validators/MarginValidator.cs
+++ SourceCode/ETDValidator/ETDValidator/Models/Validators/MarginValidator.cs
@@ -1,4 +1,9 @@
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Office2010.PowerPoint;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace ETDVAlidator.Models.Validators
 {
@@ -15,6 +20,61 @@ namespace ETDVAlidator.Models.Validators
         {
             // do parsing here, add errors and warnings
             // DocToValidate is WordprocessignDocument object to parse through 
+            
+                            // Assign a reference to the existing document body.
+                Body body = DocToValidate.MainDocumentPart.Document.Body;
+
+                MainDocumentPart mainDoc = DocToValidate.MainDocumentPart.Document.MainDocumentPart;
+                IEnumerator<HeaderPart> enumerator = mainDoc.HeaderParts.GetEnumerator();
+
+                IEnumerable<PageMargin> margins2 = mainDoc.Document.Elements<PageMargin>();
+
+                Settings settings = mainDoc.DocumentSettingsPart.Settings;
+                // Grab the results
+                // The Raw view will hold all of the text in the body
+                // Results will have each paragraph within it (A paragraph is any time a enter: so may have to be weary about line breaks in document)
+                // Results are held in arrays of 100 elements
+
+                IEnumerable<PageMargin> margins = body.Elements<PageMargin>();
+            
+                foreach (PageMargin margin in margins)
+                {
+                    var marginTop = margin.Top;
+                    var marginBottom = margin.Bottom;
+                    var marginLeft = margin.Left;
+                    var marginRight = margin.Right;
+                }
+
+                IEnumerable<Section> sections = body.Elements<Section>();
+
+                IEnumerable<Paragraph> paragraphs = body.Elements<Paragraph>();
+                foreach (Paragraph paragraph in paragraphs)
+                {
+                    ParagraphProperties paragraphProperties = paragraph.ParagraphProperties;
+
+                    if (paragraph.FirstChild != null)
+                    {
+                        IList<OpenXmlAttribute> openXmlAttributes = paragraph.GetAttributes();
+                        
+                        // Grab the text associated with it
+                        string paragraphInnerText = paragraph.InnerText;
+                        // Can use the HasChildren to see if there is another paragraph
+                        // Can use NoSpellError to check for spelling errors
+
+                        // To find out if the paragraph has styling Can also use OuterXml
+                        string paragraphRawXml = paragraph.FirstChild.InnerXml;
+                        
+                        // Uncomment to show where you can find Fonts
+                        //if (paragraph.InnerText.Contains("First Level Heading"))
+                        //{
+                            // Splitting the entire xml string into the seperate styling strings
+                            string[] innerXmlWithFont = paragraph.InnerXml.Split(">");
+                            
+                            
+                        //}
+                        
+                    }
+                }
             
             // these can be deleted - just examples
             Warnings.Add(new ComponentWarning("margin_warning_1", "warning 1 description"));
